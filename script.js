dde303b1f719723af539338d7c5b41ec1329fe55
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-06 10:11:49 -0300

    Adicionando funcionalidade do botão, requisito 3

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const sendButton = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
 const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
+const loginButton = document.getElementById('login-button');
+const loginEmail = document.getElementById('login-email');
+const loginSenha = document.getElementById('login-senha');
 
 sendButton.disabled = true;
 
@@ -18,3 +21,14 @@ function wordsCount() {
   counter.innerText = 500 - textArea.value.length;
 }
 textArea.addEventListener('input', wordsCount);
+
+function button(event) {
+  event.preventDefault();
+  if (loginEmail.value === 'tryber@teste.com' && loginSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+loginButton.addEventListener('click', button);
